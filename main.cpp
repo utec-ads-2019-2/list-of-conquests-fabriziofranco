28e4fd4f23299cab6bb59ec7ced4bc5b8606f166
Fabrizio David Franco Amayo <39867790+fabriziofranco@users.noreply.github.com>
2019-08-22 18:58:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
-
+#include <map>
+#include <string>
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main() {
+    map<string, int> Lista_de_amadas;
+    int numero_de_lineas;
+    cin>>numero_de_lineas;
+    string pais,nombre;
+    for (int i = 0; i < numero_de_lineas; ++i) {
+        cin>>pais;
+        getline (cin,nombre);
+        Lista_de_amadas[pais]++;
+    }
+    for(auto elem : Lista_de_amadas){
+        cout <<elem.first << " " << elem.second<<"\n";
+    }
     return 0;
 }
+
